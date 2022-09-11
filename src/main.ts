96b2440790690ed67980ce3e16e7068b4dfb20d6
蒋求安 <jiangqiuan@xianfengsg.com>
2022-09-11 20:22:29 +0800

    config: doc

--- src/main.ts
+++ src/main.ts
@@ -8,24 +8,29 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
 import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
+import { generateDocument } from './doc';
 
 declare const module: any;
 async function bootstrap() {
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+  // 统一响应体格式
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
   // 接口版本化管理
   app.enableVersioning({
     defaultVersion: [VERSION_NEUTRAL, '1', '2'],
     type: VersioningType.URI,
   });
-  app.useGlobalInterceptors(new TransformInterceptor());
-  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
+  generateDocument(app);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
   await app.listen(3000);
 }
 bootstrap();
